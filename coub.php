0965455c18c4b8b21a269e34e00b769108bf4625
ramiy <ramiy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-05 12:41:05 +0000

    version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/coub/trunk@1153756 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- coub.php
+++ coub.php
@@ -3,7 +3,7 @@
 Plugin Name: Coub
 Plugin URI:  https://wordpress.org/plugins/coub/
 Description: Embed coub.com looped videos into your WordPress site
-Version:     1.0
+Version:     1.1
 Author:      Rami Yushuvaev
 Author URI:  http://GenerateWP.com/
 Text Domain: coub
@@ -11,6 +11,19 @@ Domain Path: /languages
 */
 
 
+
+/*
+ * Prevent direct access to the file
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
+
+/*
+ * Register oEmbed Provider
+ */
 function coub_oembed_provider() {
 
 	wp_oembed_add_provider( '#https?://(www\.)?coub.com/view/.*#i', 'https://coub.com/api/oembed.json', true );
